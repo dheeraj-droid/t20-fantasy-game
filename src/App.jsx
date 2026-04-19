36ce1b66ab380ab4035f26b74b99ee8638922e63
Dheeraj <dheerajreddy035@gmail.com>
2026-04-20 00:36:10 +0530

    reverted prophecy

--- src/App.jsx
+++ src/App.jsx
@@ -303,7 +303,7 @@ const calculateRoundScore = (roundMatchIds, lineup, activeChip, chipNomination, 
         }
       });
       if (basePoints >= target - 50 && basePoints <= target + 50) {
-        totalRoundPoints += 250;
+        totalRoundPoints += 150;
       } else {
         totalRoundPoints -= 150;
       }
