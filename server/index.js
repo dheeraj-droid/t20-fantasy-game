fa9106ab560907edcba99b4d5360e1150d02389d
Dheeraj <dheerajreddy035@gmail.com>
2026-05-21 19:26:13 +0530

    update

--- server/index.js
+++ server/index.js
@@ -13,7 +13,8 @@ app.use(cors({
         "https://ipl-fantasy-game.vercel.app"
     ]
 }));
-app.use(express.json());
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
 
 const PORT = process.env.PORT || 5000;
 
